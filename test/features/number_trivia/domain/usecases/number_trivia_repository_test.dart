3fcc5745fb0485581429a91a499df8537bbaaf20
pratikpwr <pratiksatishpawar@gmail.com>
2021-11-27 13:30:44 +0530

    get concrete number trivia test

---
+++ test/features/number_trivia/domain/usecases/number_trivia_repository_test.dart
@@ -0,0 +1,40 @@
+import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:numtrivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:numtrivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:numtrivia/features/number_trivia/domain/usecases/get_complete_number_trivia.dart';
+
+import 'number_trivia_repository_test.mocks.dart';
+
+// class MockNumberTriviaRepository extends Mock
+//     implements NumberTriviaRepository {}
+
+@GenerateMocks([NumberTriviaRepository])
+void main() {
+  late GetConcreteNumberTrivia useCase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    useCase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  const tNumber = 1;
+  const tNumberTrivia = NumberTrivia(text: 'test', number: tNumber);
+
+  test('should get trivia for the number from the repository', () async {
+    //arrange
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
+        .thenAnswer((_) async => const Right(tNumberTrivia));
+
+    //act
+    final result = await useCase.execute(number: tNumber);
+
+    //assert
+    expect(result, const Right(tNumberTrivia));
+    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
+  });
+}
